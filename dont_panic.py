4212a3bca52834dcdabdab5a27dca7fbf86fc951
Chadwell-Clark <wcclark23@comcast.net>
2021-02-19 19:37:45 -0600

    Initial commit Dont_Panic python

---
+++ dont_panic.py
@@ -0,0 +1,12 @@
+#infinite while loop
+import os
+x = 2 
+while x == 2:
+    
+    #os.system("echo 'dont panic'")
+    os.system("say 'dont panic'") 
+    print("DON'T PANIC!!!!!")
+    print("\tDON'T PANIC!!!!!")
+    print("\t\tDON'T PANIC!!!!!")
+    print("\t\t\tDON'T PANIC!!!!!")
+    print("\t\t\t\tDON'T PANIC!!!!!")
